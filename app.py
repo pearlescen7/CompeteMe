07d7d1c6df3101f5557b9d5af5382ccbbe437dda
pearlescen7 <43743061+pearlescen7@users.noreply.github.com>
2021-01-19 18:04:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -605,4 +605,5 @@ def error(e):
     return render_template("error.html")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
